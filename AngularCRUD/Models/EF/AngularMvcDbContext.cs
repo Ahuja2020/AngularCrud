e0d12bc7e32e98c2526acac6e002ebfdb5ede9e3
Ahuja2020 <ahujasp2020@gmail.com>
2020-12-31 11:12:58 +0530

    AngularCrud

---
+++ AngularCRUD/Models/EF/AngularMvcDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AngularCRUD.Models.EF
+{
+    public class AngularMvcDbContext:DbContext
+    {
+        public AngularMvcDbContext():base("name=CyberDBEntities")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove < PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AngularCRUD/Models/EF/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AngularCRUD.Models.EF
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string ArticleTitle { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string ArticleSubtitle { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string ArticleTags { get; set; }
+        [MaxLength(32)]
+        [Required]
+        //[StringLength(32,MinimumLength=2)]
+        public string ArticleDes { get; set; }
+
+        //public DateTime DateTime { get; set; }
+    }
+}
